6eec2ce49a81f554ce1e16925ec568a1b5d4e75e
Christopher Henderson <chenderson5280@gmail.com>
2020-09-14 18:50:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,19 +1,8 @@
 def square_array(array)
-array.each do |i|
-  i **2
+count = 0
+while count < array.length do
+ Math.sqrt(array[count])
+ count+=1
 end
 array
 end
-
-
-
-
-
-
-
-#count = 0
-#while count < array.length do
- #Math.sqrt(array[count])
- #count+=1
-#end
-#array
