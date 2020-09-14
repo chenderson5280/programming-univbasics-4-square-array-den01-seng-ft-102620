b67e1a64b401d747e0f9e8e856a8ab453bc6c1dd
Christopher Henderson <chenderson5280@gmail.com>
2020-09-14 18:40:28 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,10 @@
 def square_array(array)
 count = 0
-result = []
+
 while count > array.length do
- array[count] = num
- Math.sqrt(num)
- resullt << num
+ array[count]**2
+ 
  count+=1
 end
-result
+array
 end
